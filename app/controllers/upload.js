ba88433dc6fe0536148d8d095c77fa1ce497208c
Zubeir Mohamed <zumo@solute.de>
2019-11-04 06:33:23 +0100

    Use ember file upload and setup file upload

--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { task } from 'ember-concurrency';
 import { get, set } from '@ember/object';
+import ENV from 'nilefe/config/environment';
 
 export default Controller.extend({
     uploadPhoto: task(function* (file) {
@@ -11,19 +12,21 @@ export default Controller.extend({
 
       set(meta, 'filename', get(file, 'name'));
       set(meta, 'filesize', get(file, 'size'));
+      set(meta, 'file_id', get(file, 'id'));
   
       try {
         file.readAsDataURL().then(function (url) {
           set(meta, 'url', url);
         });
   
-        let response = yield file.upload('/api/images/upload');
-        set(meta, 'url2', response.headers.Location);
+        let response = yield file.upload(`${ENV.host}/api/video/upload`);
+        set(meta, 'url', response.headers.Location);
         console.log(this.model);
         
         yield this.model.save();
       } catch (e) {
         console.error(e);
+        // document.location.reload();
       }
   }).maxConcurrency(3).enqueue(),
 

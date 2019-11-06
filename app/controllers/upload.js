c69774e6c484a7f4943bf83850297c4d204e86a5
Zubeir Mohamed <zumo@solute.de>
2019-11-06 03:19:55 +0100

    upload
    Setup form and buttons to upload files, also send to server and save to
    files

--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -2,37 +2,88 @@ import Controller from '@ember/controller';
 import { task } from 'ember-concurrency';
 import { get, set } from '@ember/object';
 import ENV from 'nilefe/config/environment';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-    uploadPhoto: task(function* (file) {
-      console.log(file);
+  toastr: service('toast'),
 
-      set(this.model, 'meta', {});
-      const { meta } = this.model;
+  init(){
+    this._super(...arguments)
+    this.files = {}
+  },
 
-      set(meta, 'filename', get(file, 'name'));
-      set(meta, 'filesize', get(file, 'size'));
-      set(meta, 'file_id', get(file, 'id'));
-  
-      try {
-        file.readAsDataURL().then(function (url) {
-          set(meta, 'url', url);
-        });
-  
-        let response = yield file.upload(`${ENV.host}/api/video/upload`);
-        set(meta, 'url', response.headers.Location);
-        console.log(this.model);
-        
-        yield this.model.save();
-      } catch (e) {
-        console.error(e);
-        // document.location.reload();
-      }
+  finishline: task(function* (files) {
+    set(this.model, 'meta', {});
+    const { meta } = this.model;
+    set(meta, 'video_originalname', get(files.video, 'name'));
+    set(meta, 'video_filesize', get(files.video, 'size'));
+    set(meta, 'video_file_id', get(files.video, 'id'));
+
+    set(meta, 'image_originalname', get(files.image, 'name'));
+    set(meta, 'image_filesize', get(files.image, 'size'));
+    set(meta, 'image_file_id', get(files.image, 'id'));
+
+    if(files.image.extension === "jpeg"){
+      set(files.image, 'name', files.image.id + ".jpg")
+    } else {
+      set(files.image, 'name', files.image.id + '.' + files.image.extension)
+    }
+
+    set(files.video, 'name', files.video.id + '.' + files.video.extension)
+
+    try {
+      const { image, video } = files;
+      image.readAsDataURL().then(function (url) {
+        set(meta, 'url', url);
+      });
+      image.readAsDataURL().then(function (url) {
+        set(meta, 'url', url);
+      });
+
+      const videoRes = yield video.upload(`${ENV.host}/api/upload`);
+      const imageRes = yield image.upload(`${ENV.host}/api/upload`);
+      set(meta, 'url', response.headers.Location);
+      
+      console.log(this.model);
+      
+      // yield this.model.save();
+    } catch (e) {
+      console.error(e);
+    }
   }).maxConcurrency(3).enqueue(),
 
   actions: {
-    uploadImage(file) {
-      get(this, 'uploadPhoto').perform(file);
+    uploadImage(image) {
+      try {
+        set(this, 'load', true);
+        set(this.files, 'image', image);
+        set(this, 'load', false);
+        this.toastr.success(`Uploaded ${image.name}`, 'Success');
+      } catch (error) {
+        console.log(error);
+        set(this, 'load', false);
+        this.toastr.error('Upload did not work', 'Error')        
+      }
+    },
+
+    uploadVideo(video) {
+      try {
+        set(this, 'load', true);
+        set(this.files, 'video', video);
+        set(this, 'load', false);
+        this.toastr.success(`Uploaded ${video.name}`, 'Success');
+      } catch (error) {
+        console.log(error);
+        set(this, 'load', false);
+        this.toastr.error('Upload did not work', 'Error')
+      }
+    },
+
+    finalize() {
+      set(this, 'load', true);
+      get(this, 'finishline').perform(this.files);
+      set(this, 'load', false);
     }
+
   }
 });

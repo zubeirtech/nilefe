ffd3ec6d3df5cc0c66af118b9b026d8f7693e4f4
Zubeir Mohamed <zumo@solute.de>
2019-11-11 22:01:50 +0100

    add integrity ta

--- config/environment.js
+++ config/environment.js
@@ -24,7 +24,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    ENV.host = 'https://api-thenile.herokuapp.com',
+    ENV.host = 'http://localhost:5000',
     ENV['ember-simple-auth'] = {
       serverTokenEndpoint: 'https://api-thenile.herokuapp.com/api/auth',
       routeAfterAuthentication: 'index'
@@ -58,7 +58,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.host = 'https://api-thenile.herokuapp.com',
+    ENV.host = 'http://localhost:5000',
     ENV['ember-simple-auth'] = {
       serverTokenEndpoint: 'https://api-thenile.herokuapp.com/api/auth',
       routeAfterAuthentication: 'index'

343b435d2037f4d0377ac2cca8306be849e9e5f9
Zubeir Mohamed <zumo@solute.de>
2019-11-11 23:48:29 +0100

    remote url for production env

--- config/environment.js
+++ config/environment.js
@@ -58,7 +58,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.host = 'http://localhost:5000',
+    ENV.host = 'https://api-thenile.herokuapp.com',
     ENV['ember-simple-auth'] = {
       serverTokenEndpoint: 'https://api-thenile.herokuapp.com/api/auth',
       routeAfterAuthentication: 'index'
